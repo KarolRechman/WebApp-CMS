82107e432c5fa1931baf7f5f96ea9d499d2d7a3a
KaRReC <39775135+KaRReC@users.noreply.github.com>
2020-05-20 08:08:48 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,111 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using Westwind.AspNetCore.LiveReload;
+
+namespace Cit_eTrike
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IConfigurationRoot configuration()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            return builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddCookie(options => { options.LoginPath = "/CMS/LoginPage"; });
+
+            services.AddMvc().AddRazorPagesOptions(options =>
+                                                    {
+                                                        options.Conventions.AddPageRoute("/ProductPage", "ProductPage/{id?}");
+                                                        options.Conventions.AddPageRoute("/PoliciesPage", "PoliciesPage/{id?}");
+                                                        options.Conventions.AddPageRoute("/CategoryPage", "CategoryPage/{id?}");
+                                                        options.Conventions.AddPageRoute("/ErrorPage", "ErrorPage/{id?}");                                                       
+                                                        //options.Conventions.AuthorizeFolder("/CMS");
+                                                        options.Conventions.AllowAnonymousToPage("/CMS/LoginPage");
+                                                    });
+            services.AddLiveReload(config =>
+            {
+                // optional - use config instead
+                //config.LiveReloadEnabled = true;
+                //config.FolderToMonitor = Path.GetFullname(Path.Combine(Env.ContentRootPath,"..")) ;
+            });
+
+            // for ASP.NET Core 3.0 add Runtime Razor Compilation
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+            services.AddMvc().AddRazorRuntimeCompilation();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                var file = Path.Combine(env.WebRootPath, "CS/UConst.html");
+                app.UseExceptionHandler(errorApp =>
+                {
+                    errorApp.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        context.Response.ContentType = "text/html";
+                        var page = await File.ReadAllTextAsync(file);
+                        await context.Response.WriteAsync(page);
+
+                        var exceptionHandlerPathFeature =
+                            context.Features.Get<IExceptionHandlerPathFeature>();
+
+                        // Use exceptionHandlerPathFeature to process the exception (for example, 
+                        // logging), but do NOT expose sensitive error information directly to 
+                        // the client.
+                    });
+                });
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseMvc();
+            app.UseAuthorization();
+            app.UseMvcWithDefaultRoute();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+
+            app.UseLiveReload();
+        }
+    }
+}
